c47df476fdd4bbda06e03cb8d077deda3b23ff5e
Owen <900920ya@gmail.com>
2022-04-10 17:28:49 +0800

    finish timers and game over

--- index.js
+++ index.js
@@ -125,6 +125,33 @@ function rectangularCollision( { rectangle1, rectangle2 } ){
     )
 }
 
+function determineWinner({player, enemy, timerID}){
+    clearTimeout(timerID)
+    document.querySelector('#displayText').style.display = 'flex'
+    if(player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie'    
+    }else if(player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    }else if(player.health < enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    }
+}
+
+let timer = 50
+let timerID
+function decreaseTimer(){
+
+    timerID = setTimeout(decreaseTimer, 1000)
+    if(timer > 0) {
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if(timer === 0){
+        determineWinner({player, enemy, timerID})   
+    }
+}
+decreaseTimer()
+
 function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -172,6 +199,11 @@ function animate(){
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
         enemy.isAttacking = false
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0){
+        determineWinner({player, enemy, timerID})
     }
 }
 animate()
